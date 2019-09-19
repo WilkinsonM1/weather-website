22a5f5590dbdb66c60a03338d4da820214bca6f3
CN72 <cn72@CN72.local>
2019-09-19 16:49:51 +0100

    added button for fahrenheit and more styling and added event listeners for keypresses for buttons

--- public/index.js
+++ public/index.js
@@ -1,32 +1,68 @@
 const title = document.getElementById("title")
-const submitBtn = document.getElementById('submit-btn')
+const cBtn = document.getElementById('c-btn')
+const fBtn = document.getElementById('f-btn')
 const input = document.getElementById('input')
 const summary = document.getElementById('summary')
 const temperature = document.getElementById('temperature')
 const place = document.getElementById('place')
+const rain = document.getElementById('rain-prob')
 
-submitBtn.addEventListener('click', async ()=>{
+
+cBtn.addEventListener('click', async ()=>{
     summary.textContent = "loading..."
     temperature.textContent =""
     let response = await fetch(`http://localhost:3000/weather?location=${input.value}`)
     let data = await response.json()
-    console.log(data)
+    console.log(data.weatherData)
     
     place.textContent = data.placeData
     summary.textContent =  data.weatherData.summary
     temperature.textContent =  `${Math.floor((data.weatherData.temperature-32)*(5/9))}°C`
+    rain.textContent = `Chance of Rain: ${data.weatherData.precipProbability*100}%`
+    
 
 })
 
-// submitBtn.addEventListener('keypress', async ()=>{
-//     if(event.which === "13"){
-//     summary.textContent = "loading..."
-//     temperature.textContent =""
-//     let response = await fetch(`http://localhost:3000/weather?location=${input.value}`)
-//     let data = await response.json()
+fBtn.addEventListener('click', async ()=>{
+    summary.textContent = "loading..."
+    temperature.textContent =""
+    let response = await fetch(`http://localhost:3000/weather?location=${input.value}`)
+    let data = await response.json()
+    console.log(data.weatherData)
     
-//     summary.textContent =  data.summary
-//     temperature.textContent =  `${Math.floor((data.temperature-32)*(5/9))}°C`
-//     }
+    place.textContent = data.placeData
+    summary.textContent =  data.weatherData.summary
+    temperature.textContent =  `${Math.floor(data.weatherData.temperature)}°F`
+    rain.textContent = `Chance of Rain: ${data.weatherData.precipProbability*100}%`
+    
 
-// })
+})
+
+window.addEventListener('keypress', async ()=>{
+    if(event.which == "13"){
+        summary.textContent = "loading..."
+        temperature.textContent =""
+        let response = await fetch(`http://localhost:3000/weather?location=${input.value}`)
+        let data = await response.json()
+        console.log(data.placeData)
+        
+        place.textContent = data.placeData
+        summary.textContent =  data.weatherData.summary
+        temperature.textContent =  `${Math.floor((data.weatherData.temperature-32)*(5/9))}°C`
+        rain.textContent = `Chance of Rain: ${data.weatherData.precipProbability*100}%`
+   }
+    })
+    window.addEventListener('keypress', async ()=>{
+        if(event.which == "32"){
+            summary.textContent = "loading..."
+            temperature.textContent =""
+            let response = await fetch(`http://localhost:3000/weather?location=${input.value}`)
+            let data = await response.json()
+            console.log(data.placeData)
+            
+            place.textContent = data.placeData
+            summary.textContent =  data.weatherData.summary
+            temperature.textContent =  `${Math.floor(data.weatherData.temperature)}°F`
+            rain.textContent = `Chance of Rain: ${data.weatherData.precipProbability*100}%`
+       }
+        })

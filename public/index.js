d57382f3eb7c81f365801b23413bbad5a04fc695
CN72 <cn72@CN72.local>
2019-09-19 15:33:28 +0100

    added input bar which user can type city to get weather data for that city

--- public/index.js
+++ public/index.js
@@ -3,14 +3,30 @@ const submitBtn = document.getElementById('submit-btn')
 const input = document.getElementById('input')
 const summary = document.getElementById('summary')
 const temperature = document.getElementById('temperature')
+const place = document.getElementById('place')
 
 submitBtn.addEventListener('click', async ()=>{
     summary.textContent = "loading..."
     temperature.textContent =""
     let response = await fetch(`http://localhost:3000/weather?location=${input.value}`)
     let data = await response.json()
+    console.log(data)
     
-    summary.textContent =  data.summary
-    temperature.textContent =  `${Math.floor((data.temperature-32)*(5/9))}°C`
+    place.textContent = data.placeData
+    summary.textContent =  data.weatherData.summary
+    temperature.textContent =  `${Math.floor((data.weatherData.temperature-32)*(5/9))}°C`
 
 })
+
+// submitBtn.addEventListener('keypress', async ()=>{
+//     if(event.which === "13"){
+//     summary.textContent = "loading..."
+//     temperature.textContent =""
+//     let response = await fetch(`http://localhost:3000/weather?location=${input.value}`)
+//     let data = await response.json()
+    
+//     summary.textContent =  data.summary
+//     temperature.textContent =  `${Math.floor((data.temperature-32)*(5/9))}°C`
+//     }
+
+// })
